cf754ac61998c85c2aaed3ea15becb81524713ff
Bheemesh <bheemesh707@gmail.com>
2017-12-18 23:10:24 +0530

    New Java Class Added

---
+++ ScannerEx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ScannerEx;
+{
+   public static void main(String[] args)throuhs Exception 
+   Scanner s=new scanner(System.in)
+   System.out.print("enter text data");
+   String data1=s.nextLine();
+System.out.print("enter same text data again ");
+   String data2=s.next();
+   System.out.print("first entered data  :"+data1);
+   
+   System.out.print("second entered data :"+data2);
+}
